c7f3a1d44b1e0b952a336fc4562c382f481d9ebf
Serhii <zarka08@gmail.com>
2022-07-24 23:01:25 +0300

    fix! search panel fixed

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="row form-row">
-    <form>
+  <div class="form-row">
+    <form class="search-form">
       <input type="text" placeholder="Search for breeds by name" />
-      <input type="submit" value="" />
+      <div class="search-submit"></div>
     </form>
     <button class="links smile-button" type="button"></button>
     <button class="links heart-button" type="button"></button>
@@ -17,101 +17,5 @@ export default {
 </script>
 
 <style scoped>
-.form-row {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-}
-
-form {
-  position: relative;
-  width: 33vw;
-  padding: 0;
-  height: 40px;
-}
-
-input[type="text"] {
-  position: relative;
-  width: 100%;
-  height: 40px;
-  padding-left: 15px;
-  border-color: transparent;
-  border-radius: 10px;
-  font-family: "Jost", sans-serif;
-  font-weight: 200;
-  color: #1d1d1d;
-}
-
-input[type="text"]:hover {
-  border: 2px solid #fbe0dc;
-}
-
-input[type="text"]:focus {
-  border: 2px solid #ff868e;
-  outline: #ff868e;
-}
-
-input[type="submit"] {
-  position: absolute;
-  right: 0.5vw;
-  top: 5px;
-  width: 30px;
-  height: 30px;
-  background-image: url("../assets/search.svg");
-  background-repeat: no-repeat;
-  background-color: #fbe0dc;
-  background-position: center;
-  border-color: transparent;
-  border-radius: 10px;
-}
-
-input[type="submit"]:hover {
-  background-color: #ff868e;
-  background-image: url("../assets/search-hover.svg");
-}
-
-button.links {
-  background-color: #fff;
-  border-radius: 10px;
-  border-color: transparent;
-  position: relative;
-  width: 40px;
-  height: 40px;
-  padding: 0;
-  background-repeat: no-repeat;
-  background-position: center;
-}
-
-button.links:hover {
-  background-color: #fbe0dc;
-}
-
-button.links:active {
-  background-color: #ff868e;
-}
-
-.smile-button {
-  background-image: url("../assets/smile-hover.svg");
-}
-
-.smile-button:active {
-  background-image: url("../assets/smile-active.svg");
-}
-
-.heart-button {
-  background-image: url("../assets/heart-hover.svg");
-}
-
-.heart-button:active {
-  background-image: url("../assets/heart-active.svg");
-}
-
-.nosmile-button {
-  background-image: url("../assets/nosmile-hover.svg");
-}
-
-.nosmile-button:active {
-  background-image: url("../assets/nosmile-active.svg");
-}
+@import url("../css/search-panel-with-icons.css");
 </style>
